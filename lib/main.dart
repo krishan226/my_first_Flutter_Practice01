96c55c3fbc4d60911000447e7f869a0fe43864c9
KRISHAN KUMAR <34978606+krishan226@users.noreply.github.com>
2025-01-29 15:52:49 +0530

    create the scalleton of firstScreen launch

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(const MyApp());
 }
 
-// stateless
-// material app
-// scaffold
+// StatelessWidget
+// Material App
+// Scaffold
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: Scaffold(),
+    );
+  }
+}
