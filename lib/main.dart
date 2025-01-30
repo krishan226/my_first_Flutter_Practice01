5ca640eac145196161af312353d68736c1f5c323
KRISHAN KUMAR <34978606+krishan226@users.noreply.github.com>
2025-01-30 12:44:16 +0530

    Start New Application
    1. Stateful-- widget --- rename as per need , called the runApp(const ---) function
    2. material App (With theme,  brigness)
    3. setHome -- Scaffold -- in Material App
    -- give navigation Title and bottom navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,4 +4,51 @@ void main() {
   runApp(const MyApp());
 }
 
-// Start New Application
+// ------  Start New Application
+// StatefulWidget
+// Material App (StateFull)
+// Scaffold
+// Nav Bar -- with title
+//BottomNavigationbar
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.light,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'My App',
+          ),
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          onDestinationSelected: (value) {
+            setState(() {
+              selectedIndex = value;
+            });
+          },
+          selectedIndex: selectedIndex,
+        ),
+      ),
+    );
+  }
+}
