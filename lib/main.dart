bca16936db883bd2a0900438af5ed4b76c568ad0
KRISHAN KUMAR <34978606+krishan226@users.noreply.github.com>
2025-01-30 11:34:31 +0530

    Create the BottomNavigation Bar with seferated value
    StateFull  ---- Screen Can refresh state
    StateLess ---- Screen Cann't refresh state
    SetState ---- Screen to refresh state

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ void main() {
   runApp(const MyApp());
 }
 
+// StateFull  ---- Screen Can refresh state
+// StateLess ---- Screen Cann't refresh state
+// SetState ---- Screen to refresh state
+
 String? title;
 
 class MyApp extends StatelessWidget {
@@ -20,47 +24,77 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(title ?? 'Krishan'),
-            centerTitle: true,
-          ),
-          drawer: Drawer(
-            child: Column(
-              children: [
-                DrawerHeader(
-                  child: Text('Menu'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.login),
-                  title: Text('Logout User'),
-                  onTap: () {},
-                ),
-                ListTile(
-                  leading: Icon(Icons.verified_user),
-                  title: Text('user'),
-                  onTap: () {},
-                ),
-              ],
+        child: MyHomePage(),
+      ),
+    );
+  }
+}
+
+// Stateful widget
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+// here
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // here we added the scafoold widget
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title ?? 'Default value'),
+        centerTitle: true,
+      ),
+      body: currentIndex == 0
+          ? Center(
+              child: Text('1'),
+            )
+          : Center(
+              child: Text('2'),
             ),
-          ),
-          bottomNavigationBar: NavigationBar(
-            destinations: [
-              NavigationDestination(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              NavigationDestination(
-                icon: Icon(Icons.person),
-                label: 'Home',
-              ),
-            ],
-            onDestinationSelected: (int value) {
-              print('Selected destination: $value');
-            },
-            selectedIndex: 0,
-          ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Text('Menu'),
+            ),
+            ListTile(
+              leading: Icon(Icons.login),
+              title: Text('Logout User'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.verified_user),
+              title: Text('user'),
+              onTap: () {},
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        onDestinationSelected: (int value) {
+          print('Selected destination: $value');
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        selectedIndex: currentIndex,
       ),
     );
   }
