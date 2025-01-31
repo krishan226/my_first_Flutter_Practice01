519bcdb05e8053c177527e121b12b078ea6724b5
KRISHAN KUMAR <34978606+krishan226@users.noreply.github.com>
2025-01-31 16:07:23 +0530

    Switch and Switch withTitle text

--- lib/Views/Pages/profilepage.dart
+++ lib/Views/Pages/profilepage.dart
@@ -10,6 +10,7 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   TextEditingController controller = TextEditingController();
   bool? isChecked = false;
+  bool isSwitched = false;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -44,6 +45,23 @@ class _ProfilePageState extends State<ProfilePage> {
             onChanged: (bool? value) {
               setState(() {
                 isChecked = value!;
+              });
+            },
+          ),
+          Switch(
+            value: isSwitched,
+            onChanged: (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
+          SwitchListTile(
+            title: Text('Switch me'),
+            value: isSwitched,
+            onChanged: (value) {
+              setState(() {
+                isSwitched = value;
               });
             },
           )
