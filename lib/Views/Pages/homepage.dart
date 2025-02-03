87bb47060b3ff94c86186f5febbc1d1c15d42d80
KRISHAN KUMAR <34978606+krishan226@users.noreply.github.com>
2025-02-03 12:27:57 +0530

    Card (Banner Card ), Create constant class and used

--- lib/Views/Pages/homepage.dart
+++ lib/Views/Pages/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_flutter/Views/Widgets/hero_widget.dart';
+import 'package:my_first_flutter/data/constants.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,6 +12,28 @@ class HomePage extends StatelessWidget {
       child: Column(
         children: [
           HeroWidget(),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 20.0),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Welcome to My First Card in Flutter App!',
+                      style: KTextStyle.titleTealText,
+                    ),
+                    Text(
+                      'This is a simple card widget.',
+                      style: KTextStyle.descriptionTealText,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/data/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class KTextStyle {
+  static const TextStyle titleTealText = TextStyle(
+    fontSize: 15.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.teal,
+    wordSpacing: 1,
+  );
+
+  static const TextStyle descriptionTealText = TextStyle(
+    fontSize: 12.0,
+  );
+}
