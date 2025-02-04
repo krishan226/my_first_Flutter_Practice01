f499a4edcd609d06e0a50b1243356d4e3b08c55b
KRISHAN KUMAR <34978606+krishan226@users.noreply.github.com>
2025-02-04 12:37:40 +0530

    ScrollView -- in Homepage

--- lib/Views/Pages/homepage.dart
+++ lib/Views/Pages/homepage.dart
@@ -9,34 +9,36 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          HeroWidget(
-            title: 'Login',
-          ),
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-            child: Card(
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Welcome to My First Card in Flutter App!',
-                      style: KTextStyle.titleTealText,
-                    ),
-                    Text(
-                      'This is a simple card widget.',
-                      style: KTextStyle.descriptionTealText,
-                    ),
-                  ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeroWidget(
+              title: 'Login',
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Welcome to My First Card in Flutter App!',
+                        style: KTextStyle.titleTealText,
+                      ),
+                      Text(
+                        'This is a simple card widget.',
+                        style: KTextStyle.descriptionTealText,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
