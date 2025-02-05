69340075f79217bdececf2d3007885ef3ebf568f
KRISHAN KUMAR <34978606+krishan226@users.noreply.github.com>
2025-02-05 12:08:02 +0530

    Saferated CardContainer, remove the bottom padding extra

--- lib/Views/Pages/homepage.dart
+++ lib/Views/Pages/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter/Views/Widgets/card_containerwidget.dart';
 import 'package:my_first_flutter/Views/Widgets/hero_widget.dart';
-import 'package:my_first_flutter/data/constants.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,34 +8,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(20.0),
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: SingleChildScrollView(
         child: Column(
           children: [
             HeroWidget(
               title: 'Login',
             ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(vertical: 20.0),
-              child: Card(
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Welcome to My First Card in Flutter App!',
-                        style: KTextStyle.titleTealText,
-                      ),
-                      Text(
-                        'This is a simple card widget.',
-                        style: KTextStyle.descriptionTealText,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+            CardContainerWidgets(
+              title: 'Welcome to my First Card in Flutter App !',
+              description: 'This is a simple card widget',
+            ),
+            CardContainerWidgets(
+              title: 'Welcome to my First Card in Flutter App !',
+              description: 'This is a simple card widget',
+            ),
+            CardContainerWidgets(
+              title: 'Welcome to my First Card in Flutter App !',
+              description: 'This is a simple card widget',
+            ),
+            CardContainerWidgets(
+              title: 'Welcome to my First Card in Flutter App !',
+              description: 'This is a simple card widget',
             ),
           ],
         ),
---
+++ lib/Views/Widgets/card_containerwidget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../data/constants.dart';
+
+class CardContainerWidgets extends StatelessWidget {
+  const CardContainerWidgets({
+    super.key,
+    required this.title,
+    required this.description,
+  });
+
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(top: 8.0),
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: KTextStyle.titleTealText,
+              ),
+              Text(
+                description,
+                style: KTextStyle.descriptionTealText,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
