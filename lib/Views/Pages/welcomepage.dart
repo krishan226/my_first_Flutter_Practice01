3da62e9800b3238c5992fbc2903fec504f102802
KRISHAN KUMAR <34978606+krishan226@users.noreply.github.com>
2025-02-05 15:10:56 +0530

    FractionallySizedBox -- wrap the colom as per the ratio of width -- 0.5 =50%

--- lib/Views/Pages/welcomepage.dart
+++ lib/Views/Pages/welcomepage.dart
@@ -15,56 +15,59 @@ class WelcomePage extends StatelessWidget {
           child: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // HeroWidget(),
-                  Lottie.asset('assets/lotties/mobilephone.json',
-                      height: 400.0),
-                  FittedBox(
-                    child: Text(
-                      'Flutter application',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25.0,
-                          letterSpacing: 5.0),
+              child: FractionallySizedBox(
+                widthFactor: 0.5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // HeroWidget(),
+                    Lottie.asset('assets/lotties/mobilephone.json',
+                        height: 400.0),
+                    FittedBox(
+                      child: Text(
+                        'Flutter application',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25.0,
+                            letterSpacing: 5.0),
+                      ),
                     ),
-                  ),
-                  FilledButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return LoginPage(
-                              title: 'User Login',
-                            );
-                          },
-                        ),
-                      );
-                    },
-                    style: FilledButton.styleFrom(
-                      minimumSize: Size(double.infinity, 40.0),
+                    FilledButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return LoginPage(
+                                title: 'User Login',
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      style: FilledButton.styleFrom(
+                        minimumSize: Size(double.infinity, 40.0),
+                      ),
+                      child: Text('Get Started'),
                     ),
-                    child: Text('Get Started'),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return WidgetTree();
-                          },
-                        ),
-                      );
-                    },
-                    style: FilledButton.styleFrom(
-                      minimumSize: Size(double.infinity, 40.0),
-                    ),
-                    child: Text('Register'),
-                  )
-                ],
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return WidgetTree();
+                            },
+                          ),
+                        );
+                      },
+                      style: FilledButton.styleFrom(
+                        minimumSize: Size(double.infinity, 40.0),
+                      ),
+                      child: Text('Register'),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
